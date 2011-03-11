b1219ab4fa35b06be5aa34f3bc0a70b8d3e7a365
VvDPzZ <vvdpzz@gmail.com>
2011-03-11 13:23:36 +0800

    init commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+gem 'devise'
+
+group :development, :test do
+  gem 'sqlite3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flipped::Application.config.secret_token = '9a324b434758ac4188d1597cfb43f2d4a51f8ccb6766ecd352869b7964bd8d39fb8702b0298880d8593782fb8c1328b1f86636d18f1429dfeb0f0aed6eaf510c'
